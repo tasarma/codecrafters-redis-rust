43f4e1c200aa6db8d2a5bf15d7008d7fcc1e0575
tasarma <tasarm7@protonmail.com>
2025-07-06 21:46:39 +0200

    Handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
 #![allow(unused_imports)]
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
-use tokio::stream;
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                handle_client(stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        tokio::spawn(async move {
+            handle_connection(socket).await;
+        });
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buf = [0u8; 1024];
+async fn handle_connection(mut socket: TcpStream) {
+    let mut buffer = [0u8; 1024];
 
     loop {
-        let bytes_read = stream.read(&mut buf).unwrap();
-
-        if bytes_read == 0 {
-            break;
+        match socket.read(&mut buffer).await {
+            Ok(0) => break,
+            Ok(_) => {
+                if let Err(e) = socket.write_all(b"+PONG\r\n").await {
+                    eprintln!("Failed to write to socket: {}", e);
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to read from socket: {}", e);
+                break;
+            }
         }
-
-        stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
